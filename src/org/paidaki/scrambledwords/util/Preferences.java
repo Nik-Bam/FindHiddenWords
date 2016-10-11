2ba63a1257faa0fd27b3b978609e5685d94a661d
Nikos Bampamis <nikos_bampamis@yahoo.com>
2016-10-11 19:19:01 +0300

    -Changed title

--- src/org/paidaki/scrambledwords/util/Preferences.java
+++ src/org/paidaki/scrambledwords/util/Preferences.java
@@ -5,7 +5,7 @@ public class Preferences {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final double DIAGONAL_FIX = Math.sqrt(2);
-    public static final String TITLE = "Scrambled Words Solver v1.0";
+    public static final String TITLE = "Hidden Words Solver v1.0";
     public static final String GITHUB_URL = "http://www.github.com/paidaki";
     public static final int DEFAULT_ROWS = 10;
     public static final int DEFAULT_COLS = 10;
